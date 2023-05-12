31684c7d26bfb32013a5a7bde077296f97e8ea28
bryony <bryonyshepherd@yahoo.co.uk>
2023-05-12 15:36:26 +0100

    get recipe by ID WIP

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import {
 // import { gatherFormData } from "./public/main";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -22,25 +22,17 @@ app.use(express.json());
 // get recipes
 app.get("/api/recipes", async function (req, res) {
   console.log(req.query, "this is the query")
-  return res.json(await getRecipes());
+  const recipeArray = await getRecipes(req.body)
+  console.log(recipeArray)
+  return res.json({ success: true, payload: recipeArray });
 })
 
 // get recipes by ID
 app.get("/api/recipes:id", async (req, res) => {
-  res.send(await getRecipeByID("4c848d48-b81e-4d6f-b45d-7b3090f4f8ef"))
+  const chosenRecipe = await getRecipeByID(req.body.id)
+  res.send({ success: true, payload: chosenRecipe });
 })
 
-// input for recipe test
-// const testRecipe = [
-//   {
-//     title: "Crunchy Frog",
-//     ingredients: ["Frog", "Crunch", "Pieces of Envy"],
-//     instructions: "Add together and have fun etc",
-//     image: "https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x375.jpg"
-//   }
-// ]
-
-// let newRecipe = gatherFormData()
 
 // creating a Recipe
 app.post("/api/recipes", async (req, res) => {
@@ -49,14 +41,6 @@ app.post("/api/recipes", async (req, res) => {
   res.send({success: true, payload: createdRecipe})
 })
 
-const testUpdateRecipe = [
-  {
-    title: "Soggy Frog",
-    ingredients: ["Frog", "Sog", "Flashes of Excellence"],
-    instructions: "`Mix` together and have fun etc",
-    image: "https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4-500x375.jpg"
-  }
-]
 
 // update recipe by ID
 app.patch("/api/recipes/:id", async (req, res) => {
